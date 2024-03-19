e306dccc66ad513c98d97eac4becf8c6ad2eecac
dario alves <dario.alves0@outlook.com>
2024-03-18 22:07:55 -0300

    add method to display all files in directory

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
-use gtk::{prelude::*, Application};
+use std::fs;
+
 
 mod counter_app;
 
+
+fn read_dir_recursive(path: &str) -> Option<bool> {
+    let dir_files = fs::read_dir(path).ok()?;
+    
+    for dir_entry in dir_files {
+        match dir_entry {
+            Ok(entry) => {
+                let is_directory = entry.path().is_dir();
+                let child_path = entry.path().as_os_str().to_str()?.to_owned();
+        
+                match is_directory {
+                    true => {
+                        read_dir_recursive(&child_path);
+                    },
+                    false => {
+                        println!("{}", child_path);
+                    }
+                }
+            },
+            Err(_) => (),
+        }
+    }
+    return Some(true);
+}
+
 fn main() {
+    read_dir_recursive("./src");
     // Create a new application with the builder pattern
     let app = Application::builder()
         .application_id("com.github.gtk-rs.examples.basic")
